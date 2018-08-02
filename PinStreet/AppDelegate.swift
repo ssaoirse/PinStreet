df9ca4ce2c5853d2f14be5795f7f188e8ccced33
Shashi Shaw <shawsaoirse@gmail.com>
2018-08-02 19:07:27 +0530

    SS - Remove unused code.

--- PinStreet/AppDelegate.swift
+++ PinStreet/AppDelegate.swift
@@ -16,14 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-//        let serviceController = WebServiceController()
-//        serviceController.fetchPinBoardItems(from: "http://pastebin.com/raw/wgkJgazE",
-//        success: { (items) in
-//
-//        },
-//        failure:{ (errMessage) in
-//
-//        })
         return true
     }
 
